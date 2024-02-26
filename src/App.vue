ec0a1222edc4fd07cf4d88b3defddd328730889a
flyc <yayayahahahaooii@gmail.com>
2024-02-26 14:34:55 +0800

    tuning style

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <script setup></script>
 
 <template>
-  <div>hello world</div>
+  <div>
+    <h3 class="title">Order Book</h3>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.title {
+  padding: 12px 12px;
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+  border-bottom-color: var(--default-text-color-for-line);
+}
+</style>
